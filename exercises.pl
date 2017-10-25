18ac2f12b0033554c55b48d16c44b54487721dd2
kadiremre <emreozcan3320@gmail.com>
2017-10-25 01:45:42 -0700

    first commit

--- exercises.pl
+++ exercises.pl
@@ -1,11 +1,34 @@
-#DAİRENİN ALANINI HESAPLAMA
-$PI = 3.141592654;
-print "lutfen daire capini giriniz \t";
-$yaricap = <STDIN>;
+#CALCULATIN OF CIRCLE FIELD
+#---------------------------
 
-if ($yaricap < 0) {
-  print "lutfen 0 dan buyuk bir sayi giriniz";
-} else {
-  $dairenin_alani = 2*$PI*$yaricap;
-  print("dairenin alan --> ".$dairenin_alani);
-}
+# $PI = 3.141592654;
+# print "please enter circle diameter\t";
+# $diameter = <STDIN>;
+#
+# if ($diameter < 0) {
+#   print "please enter a POSITIVE number";
+# } else {
+#   $circle_field = 2*$PI*$diameter;
+#   print("field of circle --> ".$circle_field);
+# }
+
+#TWO NUMBER MULTIBLAY
+#---------------------
+
+# print "enter first argument --> ";
+# $num1 = <STDIN>;
+# print "enter second number --> ";
+# $num2 = <STDIN>;
+#
+# print "multiplication of two numbers --> ".($num1*$num2);
+
+# WRITING X TIMES A STRING
+#--------------------------
+
+print  "How many times do you want to write string that will given by you? --> ";
+$times = <STDIN>;
+print  "Enter the string that you want to write --> ";
+$string = <STDIN>;
+ for (my $var = 1; $var <= $times ; $var++) {
+   print $var."-".$string;
+ }
